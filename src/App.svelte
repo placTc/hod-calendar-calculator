839e2e67e4e89df8d86a652d360d0c9c1888015d
Egor Matuk <40895729+placTc@users.noreply.github.com>
2025-07-10 10:10:07 +0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -14,7 +14,7 @@
     <hr>
     <div class="expl">
       <p>
-        The world of Hod encompasses a rich diversity of cultures, which has led, throughout history, to differing methods of reckoning the passage of years. In Baloshod, the eastern regions of Hod, societies have traditionally measured time from the end of the ancient period of “The Lawlessness,” regarding all earlier times as prehistory. In the West, where the Veras Empire once reigned unopposed, nations count years from the empire's founding, through its fall, and beyond, dividing history into three principal Eras. To reconcile these differing systems, the Universal Calendar is employed internally, adopting a single era that counts from the creation of the world.
+        The world of Hod encompasses a rich diversity of cultures, which has led, throughout history, to differing methods of reckoning the passage of years. In Baloshod, the eastern regions of Hod, societies have traditionally measured time from the end of the ancient period of “The Lawlessness,” regarding all earlier times as prehistory. In the West, where the Veras Empire once reigned unopposed, nations count years from the empire's founding, through its fall, and beyond, dividing history into three principal Eras. To reconcile these differing systems, the Universal Calendar is employed internally, adopting a single era that counts from the creation of the world. The current year that the story is set in is 261 Post-Veras, or 3647 of the Era of Law
       </p>
     </div>
   </div>
